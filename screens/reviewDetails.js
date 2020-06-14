709458b60f58a78e40d3089923bd479beec3bdf8
EvanG <evangdesigns@gmail.com>
2020-06-14 13:14:57 -0500

    passing data between screens

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -4,14 +4,11 @@ import { globalStyles } from '../styles/global';
 
 export default function ReviewDetails({ navigation }) {
 
-  const pressHandler = () => {
-    navigation.goBack();
-  }
-
   return (
     <View style={globalStyles.container}>
-      <Text>Review Details Screen</Text>
-      <Button title='Back to Home' onPress={pressHandler}/>
+      <Text>{navigation.getParam('title')}</Text>
+      <Text>{navigation.getParam('body')}</Text>
+      <Text>{navigation.getParam('rating')}</Text>
     </View>
   )
 }
